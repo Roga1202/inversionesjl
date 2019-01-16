0cc0ea8c4e6288172771193a0147c8767713c739
Daniel Urbina <josedurbinac@gmail.com>
2019-01-16 10:41:36 -0430

    tabla clientes

--- database/migrations/2019_01_14_233000_create_cliente_table.php
+++ database/migrations/2019_01_14_233000_create_cliente_table.php
@@ -14,7 +14,19 @@ class CreateClienteTable extends Migration
     public function up()
     {
         Schema::create('cliente', function (Blueprint $table) {
-            $table->increments('id');
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->bigIncrements('CL_ID');
+            $table->string('CL_primer_nombre', 20);
+            $table->string('CL_otros_nombres', 20)->nullable();
+            $table->string('CL_primer_apellido', 20);
+            $table->string('CL_otros apellidos', 20)->nullable();
+            $table->BigInteger('CL_numero_compras');
+            $table->BigInteger('CL_CI');
+            $table->integer('CL_telefono');
+            $table->string('CL_dirección', 250);
+            $table->string('CL_correo', 50);
             $table->timestamps();
         });
     }
