e04a3b3a6ee2540fe396b840a7006851abb8642f
Daniel Urbina <josedurbinac@gmail.com>
2019-01-18 13:36:38 -0430

    Creacion tabla estados

---
+++ database/migrations/2019_01_18_180538_create_table_estados.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableEstados extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estados', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_spanish2_ci';
+            $table->increments('ES_ID');
+            $table->string('ES_nombre', 50)->unique();
+            $table->timestamp('ES_inicio')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('ES_actualizacion')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estados');
+    }
+}
