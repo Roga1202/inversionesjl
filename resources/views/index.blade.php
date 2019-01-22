f44c869b656017d0964eea081d2a4be7f2839e8a
Anderson <andersontejada1997@gmail.com>
2019-01-22 18:49:49 -0400

    home frontend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,33 +1,31 @@
 
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-	<head>
-		<meta charset="utf-8">
+    <!DOCTYPE html>
+    <html lang="{{ app()->getLocale() }}">
+    <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/font-awesome/css/font-awesome.min.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/estilos.css') }}">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.6.3/css/all.css' integrity='sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/' crossorigin='anonymous'>
+    <link rel="stylesheet" type="text/css" href="Bootstrap/dist/css/bootstrap.css">
+    </head>
 
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-		<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.css') }}">
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-		<link rel="stylesheet" href="{{ asset('assets/font-awesome/css/font-awesome.min.css') }}">
-		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('assets/css/estilos.css') }}">
-        @yield('head')
-	</head>
-	
-	<body>
-		<header>
-			<h1 id="titulo"> Inversiones JL</h1>
-        </header>
-		<section>
-			<br><br><br><br><br><br><br><br><br>
-			<div class="container-fluid text-center bg2" id="opcion">
-				<h2> Bienvenidos <i class="material-icons">&#xe87e;</i> <br> Si desea anexar un pedido inicie sección dando click en el boton<h2>
-				<a href="/home" style="color: black;"><button type="submit" name="entrar" value="enviar" style="color: black; border-color: black;" class="btn btn-default" style=" border:4px solid black;">Entrar</button></a>
-			</div>
-			<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-		</section>
-        <footer class="container-fluid text-center">
-			<p><a href="https://www.instagram.com/tomate.agencia/" style="color: white;"> Elaborado por Tomate Agencia +</p></a>
-		</footer>
-	</body>
-</html>
+    <body>
+    <header>
+    <h1 id="titulo"> Inversiones JL</h1>
+    </header>
+    <section>
+    <br>
+    <div class="container-fluid text-center bg2" id="opcion">
+    <h2> Bienvenidos <i class='far fa-smile-beam'></i> <br> Si desea anexar un pedido inicie sesión dando click en el boton<h2>
+    <a href="/home" style="color: black;"><button type="submit" name="entrar" value="enviar" style="color: black; border-color: black;" class="btn btn-default" style=" border:4px solid black;">Entrar</button></a>
+    </div>
+    <br><br><br><br><br><br><br><br><br>
+    </section>
+    <footer class=" text-center bg2">
+    <p><a href="https://www.instagram.com/tomate.agencia/" style="color: white;"> Elaborado por Tomate Agencia +</p></a>
+    </footer>
+    </body>
+    </html>
